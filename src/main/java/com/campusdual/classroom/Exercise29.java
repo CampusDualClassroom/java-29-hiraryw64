fe5f37c89be5c49ea5543cdf21c2efbad772b532
hiraryw64 <hiraryw64@gmail.com>
2024-09-27 13:46:48 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,11 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try {
+            int prueba = 123 / 0;
+            System.out.println("El resultado es: " + prueba);
+        } catch (ArithmeticException e) {
+            System.err.println("No se pueden dividir números entre cero.");
+        }
     }
 }
